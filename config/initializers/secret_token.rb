16c0da9f0e6db917aa76c1bbe11119ff598a6225
Tamillharasan <tamillharasan.c@multunus.com>
2011-06-13 11:44:16 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PollHttp::Application.config.secret_token = '24c21d745ae537d70da8be2e1fc3bef6b9c8ea89836eac91f97c41bf4d2125651c07b3e1a338f951ee95adce19cd4281762ee25f4f1e4ee9f1e2124787bc56b7'
